92c4c50ae84ebb6482ef43a2825a53a3e2b72307
Diego Garcia <garciatroisi@gmail.com>
2024-07-17 11:59:28 -0300

    consultoria

--- components/works/Consultancy.tsx
+++ components/works/Consultancy.tsx
@@ -10,7 +10,7 @@ const DT = () => {
       className="w-full"
     >
       <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-        <span className="text-textGreen tracking-wide">Consultancy</span>
+        <span className="text-textGreen tracking-wide">Consultor&iacute;a</span>
       </h3>
       <p className="text-sm mt-1 font-medium text-textDark">
         Ofrecemos consultor&iacute;a en todas las disciplinas del f&uacute;tbol
