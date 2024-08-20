6fb3da735efb1112f5ea617bd058a4c3a1b7aec4
Diego Garcia <garciatroisi@gmail.com>
2024-08-20 10:23:18 -0300

    adding ivete

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  basePath: "/web",
-  assetPrefix: "/web",
-  reactStrictMode: true,
   images: {
-    // unoptimized: true
-    loader: "akamai",
-    path: "",
+    unoptimized: true,
   },
 };
 
--- public/assets/index.ts
+++ public/assets/index.ts
@@ -3,6 +3,7 @@ import profileImg from "./images/profile.jpeg";
 import diegoImg from "./images/diego.jpg";
 import fedeImg from "./images/fede.jpeg";
 import rodrigoImg from "./images/rodrigo.png";
+import iveteImg from "./images/ivete.jpeg";
 import inicial from "./images/inicial.jpg";
 import avanzado from "./images/avanzado.jpg";
 import desarrollado from "./images/desarrollado.jpg";
@@ -14,6 +15,7 @@ export {
   diegoImg,
   rodrigoImg,
   fedeImg,
+  iveteImg,
   inicial,
   avanzado,
   desarrollado,
